9be25b1787117af241eac6d4dc4adb33b541872e
Anmol <aanand@tamu.edu>
2023-09-19 21:24:09 -0500

    update

--- k_means.py
+++ k_means.py
@@ -3,13 +3,14 @@ from init_centroids import *
 
 samples, expected_clustering = generate_samples()
 
+print(expected_clustering[0:50])
+
 # [0, NUM_SAMPLES) -> [0, NUM_CLUSTERS)
-computed_clustering = np.empty(NUM_SAMPLES, dtype=np.int)
+computed_clustering = np.empty(NUM_SAMPLES, dtype=int)
 computed_centroids = np.empty((NUM_CLUSTERS, NUM_DIMENSIONS), dtype=np.float64)
 
 
-def clustering_changed():
-    changed = False
+def clustering():
     for sample_id in range(0, NUM_SAMPLES):
         min_centroid_dist = np.finfo(np.float64).max
         min_cluster_id = -1
@@ -19,14 +20,11 @@ def clustering_changed():
             if centroid_dist < min_centroid_dist:
                 min_centroid_dist = centroid_dist
                 min_cluster_id = cluster_id
-        if computed_clustering[sample_id] != min_cluster_id:
-            changed = True
         computed_clustering[sample_id] = min_cluster_id
-    return changed
 
 
 def update_centroids():
-    computed_cluster_size = np.empty(NUM_CLUSTERS, dtype=np.int)
+    computed_cluster_size = np.zeros(NUM_CLUSTERS, dtype=int)
     global computed_centroids
     computed_centroids = np.zeros((NUM_CLUSTERS, NUM_DIMENSIONS),
                                   dtype=np.float64)
@@ -42,11 +40,28 @@ def update_centroids():
 def lloyds_algorithm():
     global computed_centroids
     computed_centroids = init_centroids(samples)
-    clustering_changed()
-    while True:
+    print("*********** Initial centroids ***********")
+    print(computed_centroids)
+    clustering()
+    print(computed_clustering[0:30])
+    for _ in range(0, 50):
         update_centroids()
-        if not clustering_changed():
-            break
+        print("*********** Updated centroids ***********")
+        print(computed_centroids)
+        clustering()
+        print(computed_clustering[0:30])
+    print(expected_clustering[0:40])
+    print(computed_clustering[0:40])
+    evaluate()
+
+
+def evaluate():
+    error_metric = 0.0
+    for sample_id in range(0, NUM_SAMPLES):
+        cluster_id = computed_clustering[sample_id]
+        error_metric += euclidean_distance(samples[sample_id],
+                                           computed_centroids[cluster_id])
+    print("L1 Norm: ", error_metric)
 
 
 def main():
